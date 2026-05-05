dc8870d60f6b3f29ac1d48ed088dfdd99fb17376
andhika-97 <afajar104@gmail.com>
2026-05-05 20:01:44 +0700

    revisi tugas 11-2

---
+++ test/specs/compare.js
@@ -0,0 +1,48 @@
+const { Builder } = require('selenium-webdriver');
+const LoginAction = require('../actions/login.action')
+const {compareScreenshot} =requiere('../../utils/visual_regression_helper')
+
+describe('Login', () => {
+    let driver; 
+    let loginAction;
+
+    beforeEach(async () => {
+        driver = new Builder()
+            .forBrowser('chrome')
+            .build();
+
+        loginAction = new LoginAction(driver);
+        await loginAction.openUrl('https://www.saucedemo.com/');
+    })
+
+    afterEach(async () => {
+        await driver.quit();
+    })
+
+    it('Login with invalid credential', async () => {
+        await loginAction.inputUsername('invaliduser');
+        await loginAction.inputPassword('secret_sauce');
+        await loginAction.clickLoginButton();
+        await loginAction.assertLoginFailed('Epic sadface: Username and password do not match any user in this service');
+
+        await compareScreenshot (driver,'invalid_login');
+    });
+
+    it('Login with wrong password', async () => {
+        await loginAction.inputUsername('standard_user');
+        await loginAction.inputPassword('salah');
+        await loginAction.clickLoginButton();
+        await loginAction.assertLoginFailed('Epic sadface: Username and password do not match any user in this service');
+
+        await compareScreenshot (driver,'login_wrong_password');
+    });
+
+    it('Login with locked out user', async () => {
+        await loginAction.inputUsername('locked_out_user');
+        await loginAction.inputPassword('secret_sauce');
+        await loginAction.clickLoginButton();
+        await loginAction.assertLoginFailed('Epic sadface: Sorry, this user has been locked out.');
+
+        await compareScreenshot (driver,'lockout_user');
+    });
+})
